efde421e6100706fef0725e36c6dc15250e1c42b
Bren castaño <brecasta@gmail.com>
2024-08-18 00:18:46 +0200

    cree el jwt

---
+++ src/main/java/com/casta_dev/jwt/User/Role.java
@@ -0,0 +1,6 @@
+package com.casta_dev.jwt.User;
+
+public enum Role {
+    ADMIN,
+    USER
+}
---
+++ src/test/java/com/casta_dev/jwt/SpringBootJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casta_dev.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
